05749097c7f9367663c74af697b14a00ad0a5bf4
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-11-05 22:39:59 +0300

    This is my django project

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -6,4 +6,3 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     publication_year = models.IntegerField()
-    
--- Introduction_to_Django/LibraryProject/bookshelf/views.py
+++ Introduction_to_Django/LibraryProject/bookshelf/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def bookshelf(request):
+    return render(request, 'bookshelf/index.html')
