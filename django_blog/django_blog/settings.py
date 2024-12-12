b437d14a492cb8ac1d84f45e2aa0b96d6c4a390c
unknown <thukubrianngugi1@gmail.com>
2024-12-12 23:13:59 +0300

    myproject

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'blog/templates'],
+        'DIRS': [BASE_DIR / 'blog/static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [ BASE_DIR / "blog/static",]
-TEMPLATES[0]['DIRS'] = [BASE_DIR / 'blog/templates']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ BASE_DIR / "static",]
+STATIC_ROOT = BASE_DIR /"staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
