9d1a93f45bb10ac64a6804ef36a15aa47ba476e3
unknown <thukubrianngugi1@gmail.com>
2024-12-12 18:08:39 +0300

    advanced api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Author, Book
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .serializers import AuthorSerializer, BookSerializer
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
