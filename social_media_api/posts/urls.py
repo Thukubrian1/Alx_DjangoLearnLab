ba7fceffe696feacf1072ab8f9a8f05db662139e
unknown <thukubrianngugi1@gmail.com>
2025-01-11 17:36:47 +0300

    Social Media Api

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -9,7 +9,7 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('<int:pk>/like/', LikePostView.as_view(), name='like_post'),
-    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike_post'),
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='like_post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike_post'),
     path('feed/', FeedView.as_view(), name='feed'), 
 ]
