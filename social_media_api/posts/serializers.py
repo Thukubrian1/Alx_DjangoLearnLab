4ead93c2e26c86729a85016a9c6cfc0957faf284
unknown <thukubrianngugi1@gmail.com>
2025-01-11 15:59:42 +0300

    Social Media Api

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Post, Comment
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
@@ -20,3 +21,35 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'author', 'content', 'created_at', 'updated_at', 'post']
+
+class UserSerializer(serializers.Serializer):  # Using `serializers.Serializer` instead of `ModelSerializer`
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(max_length=150)
+    email = serializers.CharField(max_length=254, required=False, allow_blank=True)
+    first_name = serializers.CharField(max_length=30, required=False, allow_blank=True)
+    last_name = serializers.CharField(max_length=150, required=False, allow_blank=True)
+    password = serializers.CharField(write_only=True, required=True, style={"input_type": "password"})
+
+    def create(self, validated_data):
+        # Manually handle user creation
+        user = User(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        user.set_password(validated_data['password'])  # Hash password
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        # Handle updates for the User model
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
